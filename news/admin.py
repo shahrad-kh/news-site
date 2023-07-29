665bc6e4991b4e79ff5c268bab8d44e35934b213
shahrad-kh <shhrdkh5@gmail.com>
2023-07-29 11:18:53 +0330

    registering database models to admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import News, Tag
 
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'source']
+
+
+admin.site.register(Tag)
+admin.site.register(News, NewsAdmin)
