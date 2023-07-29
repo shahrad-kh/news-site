ccbfc93ad7dff3ba29f611f930614685fd361453
shahrad-kh <shhrdkh5@gmail.com>
2023-07-29 11:15:45 +0330

    creating database models

--- news/models.py
+++ news/models.py
@@ -1,2 +1,31 @@
 from django.db import models
 
+
+class Tag(models.Model):
+    """
+    Model to create tags for news
+    """
+    
+    title = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
+class News(models.Model):
+    """
+    Model to store news
+    """
+
+    title = models.CharField(max_length=50, unique=True)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    source = models.CharField(max_length=50)
+    
+    class Meta:
+        verbose_name = 'news'
+        verbose_name_plural = 'news'
+        
+    
+    def __str__(self):
+        return self.title
