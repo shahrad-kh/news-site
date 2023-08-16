2c5ae506172a9e571e3df4438427a3624e55033b
shahrad-kh <shhrdkh5@gmail.com>
2023-08-16 18:16:09 +0330

    feat: creating NewsDocument ViewSet to display news and provide searching within the news using Elasticsearch

--- news/views.py
+++ news/views.py
@@ -1,5 +1,8 @@
+from django_elasticsearch_dsl_drf.filter_backends import SearchFilterBackend
+from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 
+from .documents import NewsDocument
 from .models import News
 from .serializers import NewsSerializer
 
@@ -21,4 +24,18 @@ class NewsDetailRetrieveAPI(RetrieveAPIView):
     
     queryset  = News.objects.all()
     serializer_class = NewsSerializer
+
+
+class NewsDocumentView(DocumentViewSet):
+    """
+    Display news and provide search within news using elastic search like this:
+        .../api/news-search?search={value} or {terms}
+    """
+    document = NewsDocument
+    serializer_class = NewsDocumentSerializer
     
+    filter_backends = [
+    	SearchFilterBackend
+    ]
+    
+    search_fields = ['title', 'content', 'tags']
