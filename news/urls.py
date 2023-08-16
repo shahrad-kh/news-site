bc4a7f680b53c1731403391c3b2c056f143d14bd
shahrad-kh <shhrdkh5@gmail.com>
2023-08-16 18:17:36 +0330

    feat: creating urls for NewsDocument ViewSet using routers

--- news/urls.py
+++ news/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
+from rest_framework import routers
 
 from . import views
 
 
 app_name = 'news'
 
+router = routers.SimpleRouter(trailing_slash=False)
+router.register(r'news-search', views.NewsDocumentView, basename='News-search')
+
 urlpatterns = [
     path('news/', views.NewsListAPI.as_view(), name="news_page"),
     path('news/<int:pk>/', views.NewsDetailRetrieveAPI.as_view(), name="detail_page_by_pk"),
 ]
+urlpatterns += router.urls
+
+# To delete additional urls
+urlpatterns = [url for url in urlpatterns if not str(url.pattern).startswith('^news-search/')]
